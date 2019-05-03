76c28480f89d1c977df79e4fe34459f93be0a5e9
Allen Nguyen <allennguyen1993@gmail.com>
2019-05-03 00:04:58 -0500

    Changed m file name to make matlab compatible Signed-off-by: Allen Nguyen <allennguyen1993@gmail.com>

--- src/Explicit_Method.m
+++ src/Explicit_Method.m
@@ -22,20 +22,21 @@ u_num_current=[[u_x_0(1:N_y+1),zeros(N_y+1,N_x),u_x_2pi(1:N_y+1)];u_y_2pi];
 u_num_newer=[[u_x_0(1:N_y+1),zeros(N_y+1,N_x),u_x_2pi(1:N_y+1)];u_y_2pi]
 current_time=0;
 total_elements=numel(u_num_current);
-average_error=1; %This initial value for average error
+average_error=1; %This initial value for average error is arbitary to kickstart iteration process
 
-while aver
+while average_error>0.001
     current_time=current_time+del_t;
     
-    for i=2:
+    for i=2:N_x+1
         u_num_newer(1,i)=D*(-2*sum_del*u_num_current(1,i)+del_y_squared*(u_num_current(1,i-1)+u_num_current(1,i+1))+2*del_x_squared*u_num_current(2,i));
-        for j=2
+        for j=2:N_y+1
             u_num_newer(j,i)=D*(-2*sum_del*u_num_current(j,i)+del_y_squared*(u_num_current(j,i-1)+u_num_current(j,i+1)+del_x_squared*(u_num_current(j-1,i)+u_num_current(j+1,i);
         end
     end
     
     u_num_old=u_num_current; %When iterations continue through time, current values becomes old values
     u_num_current=u_num_newer; % And newer values become current values
-    relative_error=abs((u_num_current-u_num_old).\u_num_old)
-    avergae_error=(1/total_elements)*sum(relative_error(:))
+    relative_error=abs((u_num_current-u_num_old).\u_num_old);
+    avergae_error=(1/total_elements)*sum(relative_error(:));
+end
 
