ca89cc8e6913eddd4942bb43a3dbea2557ac90aa
Allen Nguyen <allennguyen1993@gmail.com>
2019-05-03 22:14:11 -0500

    Signed-off-by: Allen Nguyen <allennguyen1993@gmail.com>

---
+++ src/Alternating_Direction_Implicit_version_2.m
@@ -0,0 +1,21 @@
+a_x=0;
+a_y=0;
+b_x=2*pi;
+b_y=2*pi;
+N_x=input('Please enter the number of internal points in x direction:  ');
+N_y=input('Please enter the nubmer of internal points in y direction:  ');
+del_t=('please enter a value for delta t: '); %since ADI method is uncindtionally stable, del_t technically can be any thing, but error increases with del_t
+del_x=(b_x)/(N_x+1);  %N_x+1 is the number of segments resulting from the existence of N_x internal points in x diection.
+del_y=(b_y)/(N_y+1);  %N_y+1 is the number of segments resulting from the existence of N_y internal points in y drection.
+lamda=del_t/(del_x)^2;
+gamma=del_t/(del_y)^2;
+a=1+lamda; % a value is constant in space and time dimensions
+index_x=0:N_x+1;
+index_y=0:N_y+1;
+x_k=del_x*index_x; 
+y_j=del_y*index_y;
+u_x_0=((y_j).^3).';  %Boundary condition in x dimension at x=0
+u_x_2pi=(((y_j).^2).*cos(y_j)).'; %Boundary condition in x dimension at x=2pi
+u_y_2pi=u_x_0(N_y+2)+(x_k/(2*pi))*(u_x_2pi(N_y+2)-u_x_0(N_y+2)); %Boundary condition in y dimension at y=2pi
+u_num_current=zeros(N_y+2,N_x+2);
+u_num_half=zer
--- src/Explicit_Method.m
+++ src/Explicit_Method.m
@@ -4,6 +4,9 @@ b_x=2*pi;
 b_y=2*pi;
 N_x=input('Please enter the number of internal points in x direction:  ');
 N_y=input('Please enter the nubmer of internal points in y direction:  ');
+threshold_difference=input('please enter threshold value for average difference, 1e-12(recommended):  ')
+%This is threshold for average difference value under which temperature
+%distribation is no longer chaning in time and iteration process stops.
 del_x=(b_x)/(N_x+1);  %N_x+1 is the number of segments resulting from the existence of N_x internal points in x diection.
 del_y=(b_y)/(N_y+1);  %N_y+1 is the number of segments resulting from the existence of N_y internal points in y drection.
 sum_del=(del_y^2)+(del_x^2);
@@ -24,14 +27,14 @@ current_time=0;
 total_elements=numel(u_num_current);
 average_difference=1; %This initial value for average error is arbitary to kickstart iteration process
 
-while average_difference>e^-18
+while average_difference> threshold_difference
     current_time=current_time+del_t;
     
      for i=2:N_x+1 
         u_num_newer(1,i)=D*(-2*sum_del*u_num_current(1,i)+del_y_squared*(u_num_current(1,i-1)+u_num_current(1,i+1))+2*del_x_squared*u_num_current(2,i))+u_num_current(1,i);
         for j=2:N_y+1
             u_num_newer(j,i)=D*(-2*sum_del*u_num_current(j,i)+del_y_squared*(u_num_current(j,i-1)+u_num_current(j,i+1))+del_x_squared*(u_num_current(j-1,i)+u_num_current(j+1,i)))+u_num_current(j,i);
-        end
+        end 
      end 
     
     u_num_old=u_num_current; %When iterations continue through time, current values becomes old values
